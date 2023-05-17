787d6c3ff4f3a4c93e896c7bbe966de9634dba8a
Victor <victor.ireri@student.moringaschool.com>
2023-05-17 10:37:52 +0300

    completed labs

--- control_flow.rb
+++ control_flow.rb
@@ -14,3 +14,34 @@ def calculator(operation, num1, num2)
   # your code here
 end
 
+x = 10
+
+puts "x is greater than 5" if x > 5
+
+x = 10
+
+puts "x is greater than 5" if x > 5
+
+puts "x is not equal to 0" unless x == 0
+
+puts x *= 2 while x < 100
+
+day = "Monday"
+
+case day
+when "Monday"
+  puts "Start of the workweek"
+when "Friday"
+  puts "End of the workweek"
+else
+  puts "Other day of the week"
+end
+
+x = 10
+
+if x > 5
+  puts "x is greater than 5"
+else
+  puts "x is less than or equal to 5"
+end
+
